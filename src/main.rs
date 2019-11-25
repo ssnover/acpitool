ddc5b5ff6ff7ca8b4c2894b3cb6144df69dad69a
ssnover95 <sws7379@rit.edu>
2019-11-24 21:02:16 -0700

    Add AC adapter information to output

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,67 @@
+use std::path;
+
 fn main() -> std::io::Result<()> {
-    let power_supplies: Vec<acpi_client::PowerSupplyInfo> =
-        match acpi_client::get_power_supply_info() {
-            Ok(ps) => ps,
+    let power_supply_path = path::Path::new("/sys/class/power_supply");
+    let batteries: Vec<acpi_client::BatteryInfo> =
+        match acpi_client::get_battery_info(&power_supply_path) {
+            Ok(bat) => bat,
+            Err(e) => {
+                eprintln!("Application error: {}", e);
+                std::process::exit(1);
+            }
+        };
+    let adapters: Vec<acpi_client::ACAdapterInfo> =
+        match acpi_client::get_ac_adapter_info(&power_supply_path) {
+            Ok(ac) => ac,
             Err(e) => {
                 eprintln!("Application error: {}", e);
                 std::process::exit(1);
             }
         };
 
-    for ps in power_supplies {
-        if ps.is_battery {
-            display_power_supply_info(&ps);
-        }
+    for bat in batteries {
+        display_power_supply_info(&bat);
+    }
+    for ac in adapters {
+        display_ac_adapter_info(&ac);
     }
 
     Ok(())
 }
 
-fn display_power_supply_info(ps: &acpi_client::PowerSupplyInfo) {
-    if ps.is_battery {
-        let state = match &ps.state {
-            acpi_client::ChargingState::Charging => "Charging",
-            acpi_client::ChargingState::Discharging => "Discharging",
-            acpi_client::ChargingState::Full => "Full",
-        };
-        let mut seconds = ps.time_remaining.as_secs();
-        let hours = seconds / 3600;
-        seconds = seconds - hours * 3600;
-        let minutes = seconds / 60;
-        seconds = seconds - minutes * 60;
-        let not_full_string = format!(", {:02}:{:02}:{:02}", hours, minutes, seconds);
-        let charge_time_string = match &ps.state {
-            acpi_client::ChargingState::Charging => {
-                if ps.present_rate > 0 {
-                    format!("{} {}", not_full_string, "until charged")
-                } else {
-                    format!(", charging at zero rate")
-                }
+fn display_power_supply_info(bat: &acpi_client::BatteryInfo) {
+    let state = match &bat.state {
+        acpi_client::ChargingState::Charging => "Charging",
+        acpi_client::ChargingState::Discharging => "Discharging",
+        acpi_client::ChargingState::Full => "Full",
+    };
+    let mut seconds = bat.time_remaining.as_secs();
+    let hours = seconds / 3600;
+    seconds = seconds - hours * 3600;
+    let minutes = seconds / 60;
+    seconds = seconds - minutes * 60;
+    let not_full_string = format!(", {:02}:{:02}:{:02}", hours, minutes, seconds);
+    let charge_time_string = match &bat.state {
+        acpi_client::ChargingState::Charging => {
+            if bat.present_rate > 0 {
+                format!("{} {}", not_full_string, "until charged")
+            } else {
+                format!(", charging at zero rate")
             }
-            acpi_client::ChargingState::Discharging => {
-                format!("{} {}", not_full_string, "remaining")
-            }
-            _ => String::from(""),
-        };
-        println!(
-            "{}: {}, {:.1}%{}",
-            &ps.name, state, ps.percentage, charge_time_string
-        );
-    } else {
-        println!("{}", &ps.name)
-    }
+        }
+        acpi_client::ChargingState::Discharging => format!("{} {}", not_full_string, "remaining"),
+        _ => String::from(""),
+    };
+    println!(
+        "{}: {}, {:.1}%{}",
+        &bat.name, state, bat.percentage, charge_time_string
+    );
+}
+
+fn display_ac_adapter_info(ac: &acpi_client::ACAdapterInfo) {
+    let status_str = match ac.status {
+        acpi_client::Status::Online => "online",
+        acpi_client::Status::Offline => "offline",
+    };
+    println!("{}: {}", &ac.name, status_str);
 }
